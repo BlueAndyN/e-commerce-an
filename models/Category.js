db39be7e958fa115617a8f8c8a7ec328c1fa6d61
Andrew Nguyen <blueandyn@gmail.com>
2022-08-21 13:03:32 -0600

    went throught models and routed them

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,17 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // define columns, I used this as a query identifier by defining id and category columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
